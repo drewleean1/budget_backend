f0bdcfd8b473ec0cdef4bf8394d654957139a84f
drewleean1 <drewleean@gmail.com>
2023-08-20 07:59:51 -0600

    cors

--- controller.mjs
+++ controller.mjs
@@ -1,11 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import * as expenses from './model.mjs';
 import * as budget from './budget.mjs';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // EXPENSES CONTROLLER 
 app.post ('/expenses', (req, res) => {
